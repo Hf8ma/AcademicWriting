630b27ef88005a53c6861c58f9ae32850fa0f050
Hf8ma <110649481+Hf8ma@users.noreply.github.com>
2022-08-06 13:33:00 +0300

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { RegistrierungsDialogComponent } from '../registrierungs-dialog/registrierungs-dialog.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  public hide = true;
+
+  public usernameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  public emailFormControl = new FormControl('', [
+    Validators.email,
+  ]);
+
+  public passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  constructor(private readonly http: HttpClient, public dialog: MatDialog, private router: Router) {
+  }
+
+  public login(): void {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    this.http.post('<BACKEND PLACEHOLDER>/api/auth/login',
+      { username: this.usernameFormControl.value, password: this.passwordFormControl.value }, httpOptions)
+      .subscribe((user: any) => {
+        console.log(user.access_token)
+        localStorage.setItem('access_token', user.access_token);
+        localStorage.setItem('user_id', this.usernameFormControl.value);
+        this.router.navigateByUrl('editor').then(r => r);
+      });
+  }
+
+  public registrieren(): void {
+    const dialogRef = this.dialog.open(RegistrierungsDialogComponent, {
+      width: '300px',
+      height: '450px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
+}
---
+++ src/app/registrierungs-dialog/registrierungs-dialog.component.ts
@@ -0,0 +1,62 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-registrierungs-dialog',
+  templateUrl: './registrierungs-dialog.component.html',
+  styleUrls: ['./registrierungs-dialog.component.scss']
+})
+export class RegistrierungsDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<RegistrierungsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, private readonly http: HttpClient) { }
+
+  public hide = true;
+
+  public usernameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  public emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  public passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  ngOnInit(): void {
+  }
+
+  public onClose(): void {
+    this.dialogRef.close();
+  }
+
+  public registrieren(): void {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    this.http.post('<BACKEND PLACEHOLDER>/api/user',
+      {
+        username: this.usernameFormControl.value, email: this.emailFormControl.value,
+        password: this.passwordFormControl.value
+      }, httpOptions).subscribe(user => {
+        console.log(user)
+      });
+
+    this.onClose();
+  }
+}
+
+export interface DialogData {
+  username: string;
+  email: string;
+  password: string;
+}
