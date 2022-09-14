7fe4293ea0940a483d79b259e791ac935871e714
Houria Khanjar <houria.khanjar@phinexes.com>
2022-09-14 12:00:17 +0200

    - add upload pdf file

--- server/api/config.py
+++ server/api/config.py
@@ -8,6 +8,8 @@ class BaseConfig:
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = './api/files'
+    ALLOWED_EXTENSIONS = {'pdf'}
 
 
 class DevelopmentConfig(BaseConfig):
